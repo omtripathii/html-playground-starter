559a3f50847a652fcddb0a593900fe148dcbdd33
OM TRIPATHI <109409112+omtripathii@users.noreply.github.com>
2024-01-18 02:33:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const timer = document.querySelector('#time span')
+function getQuote() {
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("quote-text").textContent = data.content;
+      document.getElementById("quote-author").textContent = `- ${data.author}`;
+      
+    })
+    .catch((error) => console.error(error));
+}
 
-setInterval(() => {
-	timer.innerText = new Date().toLocaleString()
-}, 1000)
+getQuote();
